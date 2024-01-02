81a19fdceedd8f8133d474287d075b408abc6ff1
JeeveshJ7 <jeeveshj@lambdatest.com>
2024-01-02 17:48:08 +0530

    ENV Variables

--- sdk/SmartUI_SDK.js
+++ sdk/SmartUI_SDK.js
@@ -1,14 +1,18 @@
 import  { Builder, By, Key, until } from 'selenium-webdriver';
 import { smartuiSnapshot } from '@lambdatest/selenium-driver';
 
+// username: Username can be found at automation dashboard
+const USERNAME = process.env.LT_USERNAME || "<USERNAME>";
 
+// AccessKey:  AccessKey can be generated from automation dashboard or profile section
+const KEY = process.env.LT_ACCESS_KEY || "<ACCESS_KEY>";
 let capabilities = {
     platform: "catalina",
     browserName: "chrome",
     version: "latest",
     "LT:Options": {
-		"username": "<LT_USERNAME>",
-		"accessKey": "<LT_ACCESS_KEY>",
+		"username": USERNAME,
+		"accessKey": KEY,
 		"project": "<PROJECT_NAME>",
 		"w3c": true,
         name: "<TEST_NAME>", // name of the test
@@ -20,7 +24,7 @@ let capabilities = {
 
 (async function example() {
       // Setup Input capabilities
-  var gridUrl = "https://" + "<LT_USERNAME>" + ":" + "<LT_ACCESS_KEY>" + "@hub.lambdatest.com/wd/hub";
+  var gridUrl = "https://" + USERNAME + ":" + KEY + "@hub.lambdatest.com/wd/hub";
 
 
     let driver = await new Builder().usingServer(gridUrl).withCapabilities(capabilities).build();
